0767b66ac1c9193628912dfdb4079877ae27498e
Marcelofei <m.hfeitosa@hotmail.com>
2026-04-30 22:09:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -598,14 +598,6 @@ elif menu == "🏥 Escala de Plantões":
                     else: execute_query(f"DELETE FROM lancamentos WHERE id IN {ids}")
                     st.rerun()
     else: st.info("Você ainda não registrou nenhum plantão para este mês.")
-
-    st.divider()
-    
-    st.subheader("🗑️ Purgar Todo o Histórico")
-    if st.button("🚨 Apagar TODO o Histórico Global de Plantões (Banco de Dados)", type="primary"):
-        execute_query("DELETE FROM lancamentos WHERE tipo = 'Entrada' AND descricao LIKE 'Plantão %'")
-        st.success("Histórico global de plantões purgado.")
-        st.rerun()
         
     st.divider()
 
